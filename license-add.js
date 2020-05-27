915feb659dd41486700936702c099d8a1cc60de5
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2020-05-27 10:40:17 -0400

    Update license script.

--- license-add.js
+++ license-add.js
@@ -20,7 +20,7 @@
 var fs = require('fs')
 var path = require('path')
 
-const license = `/**
+const licenseJs = `/**
  *
  *    Copyright (c) 2020 Silicon Labs
  *
@@ -38,32 +38,62 @@ const license = `/**
  */
 `
 
+const licenseXml = `<!--
+Copyright (c) 2008,2020 Silicon Labs.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-->
+`
+
 // Directories that we will scan
 const directories = ['src', 'src-electron', 'test']
 
 async function processSingleFile(path) {
-  if (path.endsWith('.js') || path.endsWith('.vue')) {
-    fs.readFile(path, 'utf8', (err, data) => {
+  var isVue = path.endsWith('.vue')
+  var isJs = path.endsWith('.js')
+
+  if (!(isVue || isJs)) return
+
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) throw err
+
+    if (isJs && data.startsWith(licenseJs)) {
+      console.log(`    -  valid: ${path}: `)
+      return
+    }
+
+    if (isVue && data.startsWith(licenseXml)) {
+      console.log(`    -  valid: ${path}: `)
+      return
+    }
+
+    // Now check if we need to remove old license
+    if (data.startsWith('// Copyright') || data.startsWith('<!-- Copyright')) {
+      // We remove the firstline
+      var pos = data.indexOf('\n')
+      data = data.substr(pos + 1)
+      console.log(`    - update: ${path}`)
+    } else {
+      console.log(`    - add: ${path}: `)
+    }
+
+    // Now we write license and then data
+    var output = isJs ? licenseJs : licenseXml
+    output = output.concat(data)
+    fs.writeFile(path, output, (err) => {
       if (err) throw err
-      if (data.startsWith(license)) {
-        console.log(`    -  valid: ${path}: `)
-      } else {
-        if (data.startsWith('// Copyright')) {
-          var pos = data.indexOf('\n')
-          data = data.substr(pos + 1)
-          console.log(`    - update: ${path}`)
-        } else {
-          console.log(`    - add: ${path}: `)
-        }
-        // Now we write license and then data
-        var output = license
-        output = output.concat(data)
-        fs.writeFile(path, output, (err) => {
-          if (err) throw err
-        })
-      }
     })
-  }
+  })
 }
 
 async function readDirent(level, currentDir, dirent) {
