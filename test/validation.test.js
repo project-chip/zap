009e53537ead297436e8dbc7e12d91760432db1c
Thuc Tran <thuc.tran@silabs.com>
2020-06-01 10:55:21 -0400

    ZAPP-145 Add further unit tests to validation.js

--- test/validation.test.js
+++ test/validation.test.js
@@ -17,4 +17,32 @@ test('extractValue Functions', () => {
   expect(Validation.extractIntegerValue('5') == 5)
   expect(Validation.extractIntegerValue('0x05') == 5)
   expect(Validation.extractIntegerValue('A') == 10)
+  //float
+  expect(Validation.extractFloatValue('0.53') == 0.53)
+  expect(Validation.extractFloatValue('.53') == 0.53)
+})
+
+test('Test int bounds', () => {
+  //Integer
+  expect(Validation.checkBoundsInteger(50, 25, 60))
+  expect(!Validation.checkBoundsInteger(50, 25, 20))
+  expect(!Validation.checkBoundsInteger(50, 51, 55))
+
+  //Float
+  expect(Validation.checkBoundsFloat(35.0, 25, 50.0))
+  expect(!Validation.checkBoundsFloat(351.0, 25, 50.0))
+  expect(!Validation.checkBoundsFloat(351.0, 355, 5650.0))
+})
+
+test('Validate types', () => {
+  expect(Validation.isStringType('CHAR_STRING'))
+  expect(Validation.isStringType('OCTET_STRING'))
+  expect(Validation.isStringType('LONG_CHAR_STRING'))
+  expect(Validation.isStringType('LONG_OCTET_STRING'))
+  expect(!Validation.isStringType('FLOAT_SEMI'))
+
+  expect(Validation.isFloatType('FLOAT_SEMI'))
+  expect(Validation.isFloatType('FLOAT_SINGLE'))
+  expect(Validation.isFloatType('FLOAT_DOUBLE'))
+  expect(!Validation.isFloatType('LONG_OCTET_STRING'))
 })
