deedd1a8d442c9f0d2097678f7c41265c4ea0cc2
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2020-06-13 11:48:11 -0400

    Deal with test files and their requirement for that @jest-environment tag in the license adder.

--- license-add.js
+++ license-add.js
@@ -38,6 +38,27 @@ const licenseJs = `/**
  */
 `
 
+const licenseTestJs = `/**
+ *
+ *    Copyright (c) 2020 Silicon Labs
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ *
+ * @jest-environment node
+ */
+`
+
 const licenseXml = `<!--
 Copyright (c) 2008,2020 Silicon Labs.
 
@@ -61,11 +82,17 @@ const directories = ['src', 'src-electron', 'test']
 async function processSingleFile(path) {
   var isVue = path.endsWith('.vue')
   var isJs = path.endsWith('.js')
+  var isTestJs = path.endsWith('.test.js')
 
   if (!(isVue || isJs)) return
 
   fs.readFile(path, 'utf8', (err, data) => {
     if (err) throw err
+
+    if (isTestJs && data.startsWith(licenseTestJs)) {
+      console.log(`    -  valid: ${path}: `)
+      return
+    }
 
     if (isJs && data.startsWith(licenseJs)) {
       console.log(`    -  valid: ${path}: `)
@@ -88,7 +115,7 @@ async function processSingleFile(path) {
     }
 
     // Now we write license and then data
-    var output = isJs ? licenseJs : licenseXml
+    var output = isJs ? (isTestJs ? licenseTestJs : licenseJs) : licenseXml
     output = output.concat(data)
     fs.writeFile(path, output, (err) => {
       if (err) throw err
