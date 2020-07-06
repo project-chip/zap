3537cdf087d3367c207a029c3612dd5228199b05
Thuc Tran <thuc.tran@silabs.com>
2020-07-06 11:01:49 -0400

    ZAPP-205 ZAPP-213 Add initial tab for new UI

---
+++ src/layouts/ZclConfiguratorLayout.vue
@@ -0,0 +1,70 @@
+<!--
+Copyright (c) 2008,2020 Silicon Labs.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-->
+
+<template>
+  <q-layout view="hHh Lpr lFf">
+    <q-header
+      elevated
+      bordered
+      height-hint="500"
+      class="shadow-2 zclConfiguratorLayoutHeader"
+    >
+      <q-toolbar bordered class="shadow-2 zclConfiguratorLayoutHeader">
+        <q-toolbar-title>
+          Zigbee Clusters Configurator
+        </q-toolbar-title>
+      </q-toolbar>
+      <q-toolbar bordered class="shadow-2 zclConfiguratorLayoutHeader">
+        <div class="row">
+          <div class="col-m-4 vertical-align:middle">
+            Manufacturer Name or Code
+          </div>
+          <div></div>
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-2"
+    >
+    </q-drawer>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'ZclConfiguratorLayout',
+  methods: {},
+  data() {
+    return {
+      leftDrawerOpen: true,
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+.zclConfiguratorLayoutHeader {
+  border: 1dp white;
+  background: white;
+  color: black;
+  vertical-align: middle;
+  margin: 0px 5px 5px 0px;
+}
+</style>
