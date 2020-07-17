a04d625608ee61bf5b345ea9be0a5e4ae001ef81
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2020-07-17 10:56:31 -0400

    Add the PUT and DELETE methods.

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -22,17 +22,20 @@ import events from 'events'
 Vue.prototype.$axios = axios({ withCredentials: true })
 var eventEmitter = new events.EventEmitter()
 
+// You can set this to false to not log all the roundtrips
+const log = true
+
 // serverGet method issues a GET to a given URL,
 // then emits the response onto the event emitter.
 // The resolve promise with the response is returned
 // for possible chaining.
 Vue.prototype.$serverGet = (url) => {
-  console.log(`serverGet: ${url}`)
+  if (log) console.log(`GET → : ${url}`)
   return axios
     .get(url)
     .then((response) => {
-      console.log('get response:')
-      console.log(response)
+      if (log) console.log(`GET ← : ${url}`)
+      if (log) console.log(response)
       eventEmitter.emit(
         response.data['replyId'],
         response.data['replyId'],
@@ -43,17 +46,59 @@ Vue.prototype.$serverGet = (url) => {
     .catch((error) => console.log(error))
 }
 
-// serverGet method issues a POST to a given URL,
+// serverPost method issues a POST to a given URL,
 // then emits the response onto the event emitter.
 // The resolve promise with the response is returned
 // for possible chaining.
 Vue.prototype.$serverPost = (url, data) => {
-  console.log(`serverPost: ${url}, ${data}`)
+  if (log) console.log(`POST → : ${url}, ${data}`)
   return axios
     .post(url, data)
     .then((response) => {
-      console.log('post response')
-      console.log(response)
+      if (log) console.log(`POST ← : ${url}`)
+      if (log) console.log(response)
+      eventEmitter.emit(
+        response.data['replyId'],
+        response.data['replyId'],
+        response.data
+      )
+      return response
+    })
+    .catch((error) => console.log(error))
+}
+
+// serverPost method issues a PUT to a given URL,
+// then emits the response onto the event emitter.
+// The resolve promise with the response is returned
+// for possible chaining.
+Vue.prototype.$serverPut = (url, data) => {
+  if (log) console.log(`PUT → : ${url}, ${data}`)
+  return axios
+    .put(url, data)
+    .then((response) => {
+      if (log) console.log(`PUT ← : ${url}`)
+      if (log) console.log(response)
+      eventEmitter.emit(
+        response.data['replyId'],
+        response.data['replyId'],
+        response.data
+      )
+      return response
+    })
+    .catch((error) => console.log(error))
+}
+
+// serverPost method issues a PUT to a given URL,
+// then emits the response onto the event emitter.
+// The resolve promise with the response is returned
+// for possible chaining.
+Vue.prototype.$serverDelete = (url) => {
+  if (log) console.log(`DELETE → : ${url}`)
+  return axios
+    .delete(url)
+    .then((response) => {
+      if (log) console.log(`DELETE ← : ${url}`)
+      if (log) console.log(response)
       eventEmitter.emit(
         response.data['replyId'],
         response.data['replyId'],
