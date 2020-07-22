60f43afbb7ba959dc90636d130ce9829d8c63df0
Jing Teng <Jing.Teng@silabs.com>
2020-07-22 15:17:16 -0400

    add UC Integration prototype page.
    Offers ability to inspect/enable/disable component inside a project
    
    BUG: ZAPP-188

---
+++ src-electron/rest/uc-component.js
@@ -0,0 +1,102 @@
+/**
+ *
+ *    Copyright (c) 2020 Silicon Labs
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+/**
+ * This module provides the REST API to the generation.
+ *
+ * @module REST API: generation functions
+ */
+
+const env = require('../util/env.js')
+const restApi = require('../../src-shared/rest-api.js')
+const replyId = 'uc-tree'
+const server = 'http://127.0.0.1:9000'
+const op_tree = '/rest/clic/components/all/project/'
+const op_add = '/rest/clic/component/add/project/'
+const op_remove = '/rest/clic/component/remove/project/'
+const proj_path =
+  '_2FUsers_2Fjiteng_2FsimplicityStudioV5Platform_2Fswitch_uc_2Fswitch_uc.slcp'
+
+/**
+ *
+ *
+ * @export
+ * @param {*} db
+ * @param {*} app
+ */
+function registerUcComponentApi(db, app) {
+  const axios = require('axios')
+
+  // app.get('/uc/info', (req, res) => {})
+
+  app.get('/uc/tree', (req, res) => {
+    axios
+      .get(server + op_tree + proj_path)
+      .then(function (response) {
+        let r = {
+          replyId: replyId,
+          data: response.data,
+        }
+        // console.log(r)
+        res.send(r)
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error)
+        res.send(error.response.data)
+      })
+      .finally(function () {
+        // always executed
+      })
+  })
+
+  // app.get('/uc/details', (req, res) => {})
+  app.get('/uc/remove/:componentId', (req, res) => {
+    const { componentId } = req.params
+    axios
+      .post(server + op_remove + proj_path, {
+        componentId: componentId,
+      })
+      .then(function (r) {
+        res.send(r.data)
+      })
+      .catch(function (error) {
+        console.log(error)
+        res.send(error.response)
+      })
+  })
+
+  app.get('/uc/add/:componentId', (req, res) => {
+    const { componentId } = req.params
+    axios
+      .post(server + op_add + proj_path, {
+        componentId: componentId,
+      })
+      .then(function (r) {
+        res.send(r.data)
+      })
+      .catch(function (error) {
+        console.log(error)
+        res.send(error.response)
+      })
+  })
+
+  // component detail
+  // add/remove component
+}
+
+exports.registerUcComponentApi = registerUcComponentApi
---
+++ src/components/UcComponentSetup.vue
@@ -0,0 +1,106 @@
+<!--
+Copyright (c) 2008,2020 Silicon Labs.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-->
+<template>
+  <q-card>
+    <q-card-section>
+      <q-tree
+        :nodes="testData"
+        node-key="id"
+        tick-strategy="leaf"
+        :ticked.sync="uc.ticked"
+        :expanded.sync="uc.expanded"
+        @update:ticked="handleClick"
+      ></q-tree>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+import { QToggle } from 'quasar'
+
+export default {
+  name: 'UcComponentSetup',
+
+  data() {
+    return {
+      value: false,
+      checked: false,
+      input: [],
+
+      // q-tree attri
+      uc: {
+        last_ticked: [], // keep this to get checked/unchecked items.
+        ticked: [],
+        expanded: [],
+        a: 1,
+        tree: [],
+      },
+
+      testData: [],
+
+      last_ticked: [],
+    }
+  },
+
+  mounted() {
+    console.log('uc tree')
+    this.$serverOn('uc-tree', (event, arg) => {
+      arg.data.forEach((ele) => this.testData.push(ele))
+
+      // computed selected Nodes
+      let selected = []
+      this.testData.filter(function f(e) {
+        if (e.children) {
+          e.children.filter(f, this)
+        }
+
+        if (e.isSelected) {
+          this.push(e.id)
+        }
+      }, selected)
+      selected.forEach((e) => this.uc.ticked.push(e))
+
+      this.uc.last_ticked = this.uc.ticked
+    })
+    this.$serverGet('/uc/tree')
+  },
+
+  methods: {
+    handleClick: function (target) {
+      // let diff = .filter(x => !arr2.includes(x));
+      let enabledItems = this.uc.ticked.filter(
+        (x) => !this.uc.last_ticked.includes(x)
+      )
+      let disabledItems = this.uc.last_ticked.filter(
+        (x) => !this.uc.ticked.includes(x)
+      )
+      console.log('Enable: ' + enabledItems)
+      enabledItems.forEach(function (item) {
+        let id = item.substr(item.lastIndexOf('-') + 1)
+        this.$serverGet('/uc/add/' + id)
+      }, this)
+
+      console.log('Disable: ' + disabledItems)
+      disabledItems.forEach(function (item) {
+        let id = item.substr(item.lastIndexOf('-') + 1)
+        this.$serverGet('/uc/remove/' + id)
+      }, this)
+
+      this.uc.last_ticked = this.uc.ticked
+    },
+  },
+}
+</script>
