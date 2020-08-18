94f123f368eb4203d72c1a5fa76a868dd24fc545
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2020-08-18 08:36:10 -0400

    Add the bin converter for offset.

--- test/bin.test.js
+++ test/bin.test.js
@@ -22,20 +22,20 @@ const bin = require('../src-electron/util/bin.js')
 
 test('32-bit hex conversions', () => {
   var xN = 0x1234abcd
-  expect(bin.int32ToHex(xN)).toEqual('1234abcd')
-  expect(bin.int32ToHex(xN, true)).toEqual('cdab3412')
+  expect(bin.int32ToHex(xN)).toEqual('1234ABCD')
+  expect(bin.int32ToHex(xN, true)).toEqual('CDAB3412')
 })
 
 test('16-bit hex conversions', () => {
   var xN = 0xabcd
-  expect(bin.int16ToHex(xN)).toEqual('abcd')
-  expect(bin.int16ToHex(xN, true)).toEqual('cdab')
+  expect(bin.int16ToHex(xN)).toEqual('ABCD')
+  expect(bin.int16ToHex(xN, true)).toEqual('CDAB')
 })
 
 test('8-bit hex conversions', () => {
   var xN = 0xab
-  expect(bin.int8ToHex(xN)).toEqual('ab')
-  expect(bin.int8ToHex(xN, true)).toEqual('ab')
+  expect(bin.int8ToHex(xN)).toEqual('AB')
+  expect(bin.int8ToHex(xN, true)).toEqual('AB')
 })
 
 test('Hex to bytes conversions', () => {
@@ -49,4 +49,10 @@ test('String hex conversions', () => {
   expect(bin.hexToCBytes(xS)).toEqual(
     '0x61, 0x62, 0x63, 0x64, 0x41, 0x42, 0x43, 0x44, 0x00'
   )
+})
+
+test('Hex to binary', () => {
+  var hex = bin.int32ToHex(1234)
+  expect(hex).toBe('000004D2')
+  expect(bin.hexToBinary(hex)).toBe('00000000000000000000010011010010')
 })
