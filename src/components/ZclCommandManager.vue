81717658bbdbeb85cdfe82d58e4fcd9e8f28d095
Ezra Hale <ezra.hale@silabs.com>
2020-09-24 15:03:10 -0400

    moved class to app.sass so that it can be shared among all tables

--- src/components/ZclCommandManager.vue
+++ src/components/ZclCommandManager.vue
@@ -252,29 +252,3 @@ export default {
   },
 }
 </script>
-
-<style lang="sass">
-.my-sticky-header-table
-  /* height or max-height is important */
-  height: 77vh  //TODO: how to properly use the parents view height
-
-  .q-table__top,
-  .q-table__bottom,
-  thead tr:first-child th
-    /* bg color is important for th; just specify one */
-    background-color: #eeeeee
-
-  thead tr th
-    position: sticky
-    z-index: 1
-  thead tr:first-child th
-    top: 0
-
-  tr:nth-child(even)
-    background-color: #dddddd
-
-  /* this is when the loading indicator appears */
-  &.q-table--loading thead tr:last-child th
-    /* height of all previous header rows */
-    top: 48px
-</style>
