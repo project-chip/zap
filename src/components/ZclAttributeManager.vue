e15c1a955b2a79b496534c0f5c7df7ec87dc6b70
Ezra Hale <ezra.hale@silabs.com>
2020-09-24 12:50:26 -0400

    fix attribute loading issue, this needs to be applied to all our tables

--- src/components/ZclAttributeManager.vue
+++ src/components/ZclAttributeManager.vue
@@ -20,13 +20,18 @@ limitations under the License.
   add action to edit button
 -->
 <template>
-  <div v-show="attributeData.length > 0">
+  <div
+    v-show="attributeData.length > 0"
+    style="overflow: hidden; height: 100%;"
+  >
     <q-table
+      class="my-sticky-header-table"
       :data.sync="attributeData"
       :columns="columns"
       row-key="<b>name</b>"
       dense
       flat
+      virtual-scroll
       binary-state-sort
       :pagination.sync="pagination"
       :sort-method="customAttributeSort"
@@ -514,7 +519,7 @@ export default {
       editableDefaults: {},
       editableStorage: {},
       pagination: {
-        rowsPerPage: 50,
+        rowsPerPage: 0,
         sortBy: 'attrID',
       },
       columns: [
@@ -599,11 +604,25 @@ export default {
 }
 </script>
 
-<style scoped>
-tr:nth-child(even) {
-  background-color: #dddddd;
-}
-th {
-  background-color: #dddddd;
-}
+<style lang="sass">
+.my-sticky-header-table
+  /* height or max-height is important */
+  height: 75vh
+
+  .q-table__top,
+  .q-table__bottom,
+  thead tr:first-child th
+    /* bg color is important for th; just specify one */
+    background-color: #eeeeee
+
+  thead tr th
+    position: sticky
+    z-index: 1
+  thead tr:first-child th
+    top: 0
+
+  /* this is when the loading indicator appears */
+  &.q-table--loading thead tr:last-child th
+    /* height of all previous header rows */
+    top: 48px
 </style>
