66851dbbabd37799d226c9c437fd9781fc81aea0
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2020-11-17 09:24:50 -0400

    Give more meaningful name to server side.

--- src-electron/server/http-server.js
+++ src-electron/server/http-server.js
@@ -27,7 +27,7 @@ const session = require('express-session')
 const env = require('../util/env.js')
 const querySession = require('../db/query-session.js')
 const util = require('../util/util.js')
-const webSocket = require('./websocket.js')
+const webSocket = require('./ws-server.js')
 
 const restApiModules = [
   '../rest/admin.js',
