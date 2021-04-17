e2d121dfaa45c3e3237554eb07ee318050a4d152
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2021-04-16 22:15:13 -0400

    Restore styles to the headers.

--- src/layouts/ZclConfiguratorLayout.vue
+++ src/layouts/ZclConfiguratorLayout.vue
@@ -17,8 +17,13 @@ limitations under the License.
 <template>
   <div>
     <q-layout view="hHh Lpr lFf">
-      <q-header elevated bordered height-hint="500" class="shadow-2">
-        <q-toolbar bordered class="shadow-2">
+      <q-header
+        elevated
+        bordered
+        height-hint="500"
+        class="shadow-2 zclConfiguratorLayoutHeader"
+      >
+        <q-toolbar bordered class="shadow-2 zclConfiguratorLayoutHeader">
           <q-toolbar-title v-on:click.ctrl="showVersion">
             Zigbee Cluster Configurator
           </q-toolbar-title>
