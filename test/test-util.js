21b1c01703c5172081081081b6b4b1a9ea896a7c
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2021-06-06 14:15:25 -0400

    Fix the timeouts for unit tests.

--- test/test-util.js
+++ test/test-util.js
@@ -62,4 +62,4 @@ exports.totalServerAttributeCount = 2954
 exports.totalSpecCount = 39
 exports.totalEnumCount = 209
 exports.totalEnumItemCount = 1595
-exports.longTimeout = 12000
+exports.longTimeout = 13000
