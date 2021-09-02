9a48c8819348b313b861dc5c9a0edc4f0af56e37
Jing Teng <jing.teng@silabs.com>
2021-09-02 14:41:19 -0400

    do not ping backend / studio when in standalone/electron mode
    BUG: ZAPP-650

--- src/util/common-mixin.js
+++ src/util/common-mixin.js
@@ -123,17 +123,27 @@ export default {
     },
 
     /**
+     * Whether ZAP is running in standalone / Electron mode or not.
+     * @returns
+     */
+    standaloneMode() {
+      return this.$store.state.zap.standalone
+    },
+
+    /**
      * Enable components by pinging backend, which pings Studio jetty server.
      * @param {*} params
      */
     updateSelectedComponentRequest(params) {
-      this.$store
-        .dispatch('zap/updateSelectedComponent', params)
-        .then((response) => {
-          if (response.status != http.StatusCodes.OK) {
-            console.log('Failed to update selected components!')
-          }
-        })
+      if (!standaloneMode()) {
+        this.$store
+          .dispatch('zap/updateSelectedComponent', params)
+          .then((response) => {
+            if (response.status != http.StatusCodes.OK) {
+              console.log('Failed to update selected components!')
+            }
+          })
+      }
     },
 
     /**
