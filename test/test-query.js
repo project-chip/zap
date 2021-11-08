8878d61548fd9eb1afea4bae771c80c0175b060c
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2021-11-08 12:35:18 -0500

    Rerun licence script, since I was reminded of it.

--- test/test-query.js
+++ test/test-query.js
@@ -1,11 +1,11 @@
 /**
  *
- *    Copyright (c) 2020 Silicon Labs
+ *    Copyright (c) 2021 Silicon Labs
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
-
+ *
  *        http://www.apache.org/licenses/LICENSE-2.0
  *
  *    Unless required by applicable law or agreed to in writing, software
