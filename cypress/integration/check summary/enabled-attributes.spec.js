12e7f12b3dfebb7a8591f59d0459135b5098a0ad
Mehrad Malayeri <mehrad.malayeri@gmail.com>
2022-01-18 14:32:16 +0330

    finalzing cypress with CI

--- cypress/integration/check summary/enabled-attributes.spec.js
+++ cypress/integration/check summary/enabled-attributes.spec.js
@@ -9,22 +9,21 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 describe('Testing enabled attributes amount', () => {
   it('create a new endpoint and get amount of enabled attributes', () => {
     cy.visit('http://localhost:8080/?restPort=9070#/')
-    cy.addEndpoint('Billing Unit (0x0203)', 'General').then(() => {
-      cy.wait(1000)
-      cy.get(':nth-child(7) > .text-right').then(($div) => {
-        const num1 = parseFloat($div.text())
-        cy.gotoAttributePage('', 'General')
-        cy.get(
-          '#qvs_116 > :nth-child(2) > :nth-child(2) > .q-mt-xs > .q-toggle__inner'
-        ).click()
-        cy.get('.router-link-active')
-          .contains('Back')
-          .click()
-          .then(() => {})
-        cy.get(':nth-child(7) > .text-right').then(($div2) => {
-          const num2 = parseFloat($div2.text())
-          expect(num2).to.eq(num1 + 1)
-        })
+    cy.addEndpoint('Billing Unit (0x0203)', 'General')
+    cy.wait(2000)
+    cy.get(':nth-child(7) > .text-right').then(($div) => {
+      const num1 = parseFloat($div.text())
+      cy.gotoAttributePage('', 'General')
+      cy.get(
+        '#qvs_116 > :nth-child(2) > :nth-child(2) > .q-mt-xs > .q-toggle__inner'
+      ).click()
+      cy.get('.router-link-active')
+        .contains('Back')
+        .click()
+        .then(() => { })
+      cy.get(':nth-child(7) > .text-right').then(($div2) => {
+        const num2 = parseFloat($div2.text())
+        expect(num2).to.eq(num1 + 1)
       })
     })
   })
