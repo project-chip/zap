43c8117d9f1c33dc7ab34cc829d0903240bc534d
sahar kargar <sahar.kaargar@gmail.com>
2022-01-18 13:34:23 +0330

    new tests and commands

---
+++ cypress/integration/check summary/enabled-attributes.spec.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false
+})
+
+describe('Testing enabled attributes amount', () => {
+  it('create a new endpoint and get amount of enabled attributes', () => {
+    cy.visit('http://localhost:8080/?restPort=9070#/')
+    cy.addEndpoint('Billing Unit (0x0203)', 'General').then(() => {
+      cy.wait(1000)
+      cy.get(':nth-child(7) > .text-right').then(($div) => {
+        const num1 = parseFloat($div.text())
+        cy.gotoAttributePage('', 'General')
+        cy.get(
+          '#qvs_116 > :nth-child(2) > :nth-child(2) > .q-mt-xs > .q-toggle__inner'
+        ).click()
+        cy.get('.router-link-active')
+          .contains('Back')
+          .click()
+          .then(() => {})
+        cy.get(':nth-child(7) > .text-right').then(($div2) => {
+          const num2 = parseFloat($div2.text())
+          expect(num2).to.eq(num1 + 1)
+        })
+      })
+    })
+  })
+})
---
+++ cypress/integration/endpoints/edit-endpoint.spec.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false
+})
+
+describe('Testing Editing endpoints', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:8080/?restPort=9070#/')
+  })
+  it('create a new endpoint', () => {
+    cy.addEndpoint('Billing Unit (0x0203)')
+  })
+  it('edit endpoint', () => {
+    cy.get('button').contains('Edit').click()
+    cy.get(
+      '.q-form > .q-select > .q-field__inner > .q-field__control > .q-field__control-container'
+    ).click()
+    cy.wait(1000)
+    cy.get('div').contains('CBA Config Tool (0x0005)').click()
+    cy.wait(1000)
+    cy.get('button').contains('Save').click()
+    cy.wait(1000)
+  })
+  it('Check if edit is successfull', () => {
+    cy.get('aside').children().should('contain', 'CBA Config Tool (0x0005)')
+  })
+})
