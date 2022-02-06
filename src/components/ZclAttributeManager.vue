be6a7ee3fef17d943b748625dd92ce847e9b9741
Mehrad Malayeri <mehrad.malayeri@gmail.com>
2022-02-06 08:29:20 +0330

    https://github.com/project-chip/zap/issues/354

--- src/components/ZclAttributeManager.vue
+++ src/components/ZclAttributeManager.vue
@@ -196,7 +196,18 @@ limitations under the License.
                   selectedCluster.id
                 )
               "
-            />
+            >
+            <template v-slot:append v-if="props.row.isNullable">
+              <q-btn color="secondary" label="Null" size="sm" @click="
+                handleLocalChange(
+                  null,
+                  'defaultValue',
+                  props.row,
+                  selectedCluster.id
+                )
+              " />
+            </template>
+          </q-input>
           </q-td>
         </q-tr>
       </template>
