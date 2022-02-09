66c7fcbb1b4bb4a4d90bb59e3581d85eec271085
Jing Teng <jing.teng@silabs.com>
2022-02-09 13:08:56 -0500

    Cypress custom xml unit test

--- cypress/integration/custom_xml/custom_xml.spec.js
+++ cypress/integration/custom_xml/custom_xml.spec.js
@@ -16,13 +16,13 @@ describe('Check ZCL Extension functionalities', () => {
       .then(function (window) {
         window[rendApi.GLOBAL_SYMBOL_EXECUTE](
           rendApi.id.open,
-          path.join(__dirname, 'test.zap')
+          path.join(__dirname, 'custom_xml.zap')
         )
       })
       .then(() => {
         cy.get(
           ':nth-child(3) > .q-card > .q-list > :nth-child(2) > :nth-child(2)'
-        ).should('contain', 'Vendor Device Type Name (0xDEAD)')
+        ).should('contain', 'Bead Device Type Name (0xDEAD)')
 
         cy.get(
           ':nth-child(3) > .q-card > .q-list > :nth-child(4) > :nth-child(2)'
@@ -34,30 +34,11 @@ describe('Check ZCL Extension functionalities', () => {
           '.col-4 > .q-field__inner > .q-field__control > .q-field__control-container > input'
         )
           .clear({ force: true })
-          .type('Bridge', { force: true })
-        cy.get('.q-table--col-auto-width').should('contain', 'Bridge')
-        cy.get('.q-tr > :nth-child(3)').should('contain', 'Server')
-        cy.get('.q-tr > :nth-child(4)').should('contain', '0xFD00')
-        cy.get('.q-tr > :nth-child(5)').should('contain', '0x1022')
-
-        let cluster = 'Bridge'
-        cy.get('.q-tr > :nth-child(7) > .q-btn').click({ force: true })
-        cy.get(
-          ':nth-child(5) > :nth-child(2) > .q-mt-xs > .q-toggle__inner'
-        ).find('input')
-        // .should('be.checked') NOT WORKING
-
-        cy.get('.router-link-active').click()
-
-        cy.get(
-          '.col-4 > .q-field__inner > .q-field__control > .q-field__control-container > input'
-        )
-          .clear({ force: true })
-          .type('XBB Cluster', { force: true })
-        cy.get('.q-table--col-auto-width').should('contain', 'XBB Cluster')
-        cy.get('.q-tr > :nth-child(3)').should('contain', 'Server')
-        cy.get('.q-tr > :nth-child(4)').should('contain', '0xFD02')
-        cy.get('.q-tr > :nth-child(5)').should('contain', '0x111D')
+          .type('Bead', { force: true })
+        cy.get('.q-table--col-auto-width').should('contain', 'Custom Bead')
+        cy.get('.q-tr > :nth-child(4)').should('contain', '0xFCAA')
+        cy.get('.q-tr > :nth-child(5)').should('contain', '0xBEAD')
+        cy.get('.q-tr > :nth-child(6)').should('contain', 'Client & Server')
       })
   })
 })
