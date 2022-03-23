94b81fbf0fe4c77d492463318cbf0589ac4fbf34
Jing T <jing.teng@silabs.com>
2022-03-23 11:11:11 -0400

    instrument codebase / compile code coverage report from Jest and Cypress tests (#450)
    * instrument codebase via babel-plugin-istanbul for Cypress
    * direct jest report to jest-coverage / cypress report to cypress-coverage 
    * combine/display code coverage report from all tests via "npm run report"
    
    BUG:  ZAPP-774

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -19,4 +19,12 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+
+  require('@cypress/code-coverage/task')(on, config)
+
+  // add other tasks to be registered here
+
+  // IMPORTANT to return the config object
+  // with the any changed environment variables
+  return config
 }
