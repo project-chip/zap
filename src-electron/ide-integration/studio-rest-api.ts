15f775cf404f3294ad3dc099b0eff0d9a9b4dc25
Paul Regan <paregan@silabs.com>
2022-06-28 11:28:19 -0400

    fixing type script

--- src-electron/ide-integration/studio-rest-api.ts
+++ src-electron/ide-integration/studio-rest-api.ts
@@ -29,10 +29,12 @@ import * as dbTypes from '../../src-shared/types/db-types'
 import * as querySession from '../db/query-session.js'
 const wsServer = require('../server/ws-server.js')
 const dbEnum = require('../../src-shared/db-enum.js')
+const queryUpgrade = __importStar(require('../db/query-upgrade.js'))
 import * as ucTypes from '../../src-shared/types/uc-component-types'
 import * as dbMappingTypes from '../types/db-mapping-types'
 import * as http from 'http-status-codes'
 import zcl from './zcl.js'
+
 import * as sqlite from 'sqlite3'
 
 const localhost = 'http://localhost:'
@@ -341,6 +343,7 @@ async function sendSessionUpgrade(db: dbTypes.DbType) {
           category: dbEnum.wsCategory.upgrade,
           payload: status,
         })
+        await queryUpgrade.updateUpgrade(db, 0, 'none')
       }
     }
   }
