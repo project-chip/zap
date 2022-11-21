758816a2cd001d94048bce22a9caeeb9227167f4
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2022-11-21 09:21:26 -0500

    Fix the failure with camelcasing logic. (#827)
    Minor problem in camelcasing logic caused a failure in one Matter generation.

--- src-electron/generator/matter/app/zap-templates/templates/app/helper.js
+++ src-electron/generator/matter/app/zap-templates/templates/app/helper.js
@@ -398,6 +398,8 @@ function asCamelCase(label, firstLower, preserveAcronyms) {
         return token;
       }
 
+      if (token.length === 0) return token;
+
       let newToken =
         index == 0 && firstLower
           ? token[0].toLowerCase()
