de38ebd95374a8c9294b3080deb851356889fd44
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2023-02-28 09:27:05 -0500

    Some further cleanup.

--- test/gen-dotdot.test.js
+++ test/gen-dotdot.test.js
@@ -28,7 +28,7 @@ const testUtil = require('./test-util.js')
 const queryPackage = require('../src-electron/db/query-package.js')
 
 let db
-const testFile = path.join(__dirname, 'resource/three-endpoint-device.zap')
+const testFile = testUtil.zigbeeTestFile.threeEp
 let sessionId
 let templateContext
 let zclContext
