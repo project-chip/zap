3b7ac93e9a9f3b5bcf9657f8da27080e25844134
tbrkollar <116916192+tbrkollar@users.noreply.github.com>
2023-04-18 12:39:20 +0200

    add previewtab function, fix toolbar for studio

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,8 +4,9 @@
       <ZCLToolbar />
     </q-header>
     <q-drawer
+      v-if="!showPreviewTab"
       class="bg-glass"
-      v-model="drawer"
+      v-model="leftDrawerOpen"
       show-if-above
       :width="300"
       :breakpoint="500"
@@ -26,7 +27,64 @@
         </router-view>
       </q-scroll-area>
     </q-drawer>
-
+    <q-drawer
+      :width="$q.screen.width * 0.4"
+      bordered
+      v-model="showPreviewTab"
+      side="right"
+      :breakpoint="0"
+      class="bg-glass column"
+    >
+      <!-- <div
+        v-on:click.ctrl="showVersion"
+        v-if="showPreviewTab && this.endpointId[this.selectedEndpointId]"
+      >
+        <q-select
+          class="q-mx-sm q-mt-sm"
+          filled
+          :options="endpoints"
+          :model-value="selectedEndpointId"
+          label="Endpoint"
+          emit-value
+          map-options
+          @update:model-value="setSelectedEndpoint($event)"
+        />
+      </div> -->
+      <q-btn-dropdown
+        no-caps
+        color="primary"
+        :label="generationButtonText"
+        dropdown-icon="change_history"
+        class="q-ma-sm"
+        data-test="select-file-in-preview"
+      >
+        <q-list>
+          <q-item
+            v-for="(file, i) in generationFiles"
+            :key="i"
+            clickable
+            v-close-popup
+            @click="
+              () => {
+                generationButtonText = file.category
+                getGeneratedFile(file.category)
+              }
+            "
+            :label="generationButtonText"
+          >
+            <q-item-section>
+              <q-item-label>{{ file.category }}</q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-btn-dropdown>
+      <div class="col column bg-white q-mx-sm">
+        <q-scroll-area class="fit" ref="generationScroll">
+          <pre class="q-ma-sm">{{ generationData }}</pre>
+          <q-scroll-observer @scroll="onScroll" />
+        </q-scroll-area>
+      </div>
+    </q-drawer>
     <q-page-container>
       <router-view v-slot="{ Component }">
         <transition mode="out-in" name="slide-down">
@@ -38,6 +96,7 @@
 </template>
 <script>
 import ZCLToolbar from '../components/ZCLToolbar.vue'
+const restApi = require(`../../src-shared/rest-api.js`)
 
 export default {
   components: {
@@ -45,24 +104,81 @@ export default {
   },
   name: 'MainLayout',
   data() {
-    return {}
+    return {
+      drawerRight: false,
+      generationFiles: '',
+      generationData: 'Generated Data',
+      generationButtonText: 'Select File',
+      currentFile: '',
+      scrollInfo: {},
+      scrollHeight: '',
+      clientHeight: '',
+      scrollTop: '',
+      index: 0,
+      maxIndex: 0,
+      generationDirectory: '',
+    }
+  },
+  computed: {
+    showPreviewTab: {
+      get() {
+        return this.$store.state.zap.showPreviewTab
+      },
+      set() {
+        return this.$store.dispatch('zap/togglePreviewTab')
+      },
+    },
+    leftDrawerOpen: {
+      get() {
+        return this.$store.state.zap.leftDrawerOpenState
+      },
+      set(newLeftDrawerOpenState) {
+        this.$store.dispatch('zap/setLeftDrawerState', newLeftDrawerOpenState)
+      },
+    },
+  },
+  created() {
+    if (this.$serverGet != null) {
+      this.getGeneratedFiles()
+    }
   },
   methods: {
-    minimize() {
-      if (process.env.MODE === 'electron') {
-        //
-      }
+    getGeneratedFiles() {
+      this.$serverGet(restApi.uri.preview).then((result) => {
+        this.generationFiles = result.data
+      })
     },
-
-    toggleMaximize() {
-      if (process.env.MODE === 'electron') {
-        //
-      }
+    getGeneratedFile(fileName, index = 1) {
+      this.$serverGet(restApi.uri.preview + fileName + '/' + index)
+        .then((result) => {
+          this.generationData = result.data['result']
+          this.maxIndex = result.data['size']
+          this.index = index
+          this.currentFile = fileName
+          this.$refs.generationScroll.setScrollPosition('vertical', 0)
+        })
+        .catch((err) => console.log('Server Get:' + err))
     },
-
-    closeApp() {
-      if (process.env.MODE === 'electron') {
-        //
+    onScroll(info) {
+      this.scrollInfo = info
+      const scrollArea = this.$refs.generationScroll
+      const scrollTarget = scrollArea.getScrollTarget()
+      this.scrollHeight = scrollTarget.scrollHeight
+      this.clientHeight = scrollTarget.clientHeight
+      this.scrollTop = scrollTarget.scrollTop
+      if (
+        this.scrollInfo.direction === 'down' &&
+        this.scrollHeight - this.scrollTop === this.clientHeight &&
+        this.maxIndex > this.index
+      ) {
+        this.index = this.index + 1
+        this.$serverGet(
+          restApi.uri.preview + this.currentFile + '/' + this.index
+        )
+          .then((result) => {
+            this.generationData = this.generationData + result.data['result']
+          })
+          .catch((err) => console.log('Server Get:' + err))
       }
     },
   },
