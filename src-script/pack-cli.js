4861f737054d58f86c250b760c6404ad320b92b9
Jing T <jing.teng@silabs.com>
2023-05-08 20:54:18 -0400

    create linux arm64 release package
    * support for creating linux arm64 artifacts
    
    1) NOTE: workaround for arm64 with pkg via "--no-bytecode --public"
    
    the packaging utilility (pkg) has following issues:
    
    * bytecode generation fails for arm64:
      Warning Failed to make bytecode node16-arm64 for file /snapshot/zap/node_modules/...
    
      "--no-bytecode" flag is used to skip the bytecode generation
    
    * --no-bytecode flag is broken:
      "Error! --no-bytecode and no source breaks final executable"
    
      "--public" flag is used as a workaround
      ref: https://github.com/vercel/pkg/issues/1139
    
    2) update mac artifacts to be platform/architecture specific
    
    3) pkg: version bump to pick up codesign error for macOS arm64

--- src-script/pack-cli.js
+++ src-script/pack-cli.js
@@ -1,38 +1,61 @@
 'use strict'
 
-// Utilty script for adding zap-cli binaries to zap binaries generated via "pack:$platform" NPM targets
+// Utilty script for adding ${cli} binaries to zap binaries generated via "pack:$platform" NPM targets
 
 const zip = require('../node_modules/7zip-bin/index')
 const scriptUtil = require('./script-util.js')
-const fs = require('fs')
+const fsp = require('fs/promises')
 const yargs = require('yargs/yargs')
 const { hideBin } = require('yargs/helpers')
-
 const argv = yargs(hideBin(process.argv)).argv
 const baseDir = `${__dirname}/../`
-let args = []
+const dist = `${baseDir}/dist`
 console.log(argv)
 
-let platform
-if (argv.p) {
-  platform = argv.p
-} else {
-  platform = process.platform
+const platform = argv.p ? argv.p : process.platform
+
+async function addToZip(dst, filename) {
+  await scriptUtil.executeCmd({}, zip.path7za, [
+    'a',
+    `${dist}/${dst}`,
+    `${dist}/${filename}`,
+  ])
 }
 
-if (platform.includes('darwin') || platform.includes('mac')) {
-  fs.rename(`${baseDir}/dist/zap-macos`, `${baseDir}/dist/zap-cli`, () => {})
-  args.push('a', `${baseDir}/dist/zap-mac.zip`, `${baseDir}/dist/zap-cli`)
-} else if (platform.includes('win')) {
-  fs.rename(
-    `${baseDir}/dist/zap-win.exe`,
-    `${baseDir}/dist/zap-cli.exe`,
-    () => {}
-  )
-  args.push('a', `${baseDir}/dist/zap-win.zip`, `${baseDir}/dist/zap-cli.exe`)
-} else {
-  fs.rename(`${baseDir}/dist/zap-linux`, `${baseDir}/dist/zap-cli`, () => {})
-  args.push('a', `${baseDir}/dist/zap-linux.zip`, `${baseDir}/dist/zap-cli`)
+async function rename(oldPath, newPath) {
+  await fsp.rename(`${dist}/${oldPath}`, `${dist}/${newPath}`)
 }
 
-scriptUtil.executeCmd({}, zip.path7za, [...args])
+async function main() {
+  if (platform.includes('darwin') || platform.includes('mac')) {
+    let file = 'zap-macos'
+    let cli = `zap-cli`
+
+    await rename(`${file}`, cli)
+    await addToZip(`${file}-x64.zip`, cli)
+
+    // NOTE: pkg support for macos-arm64 is experimental
+    // await rename(`${file}-arm64`, cli)
+    // await addToZip(`${file}-arm64.zip`, cli)
+  } else if (platform.includes('win')) {
+    let file = 'zap-win'
+    let cli = `zap-cli.exe`
+
+    await rename(`${file}-x64.exe`, cli)
+    await addToZip(`${file}-x64.zip`, cli)
+
+    await rename(`${file}-arm64.exe`, cli)
+    await addToZip(`${file}-arm64.zip`, cli)
+  } else if (platform.includes('linux')) {
+    let file = 'zap-linux'
+    let cli = `zap-cli`
+
+    await rename(`${file}-x64`, cli)
+    await addToZip(`${file}-x64.zip`, cli)
+
+    await rename(`${file}-arm64`, cli)
+    await addToZip(`${file}-arm64.zip`, cli)
+  }
+}
+
+main()
