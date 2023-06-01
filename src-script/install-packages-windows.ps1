592d8baf137cbd25b11e0d60bde9186e999b1fb5
Ethan Zhou <yanchengzhou020830@gmail.com>
2023-06-01 09:36:23 -0400

    update Powershell comments

--- src-script/install-packages-windows.ps1
+++ src-script/install-packages-windows.ps1
@@ -1,6 +1,8 @@
-# Packages you have to install on Windows to get source build to compile via npm install.
-# Under the zap repo directory, to run the script: ./src-script/install-packages-windows
+# Packages you have to install on Windows to get source build to compile via npm install
+# To run this PowerShell script, navigate to the root directory of ZAP repo in PowerShell and execute the following command:
+# ./src-script/install-packages-windows
 
+# install Chocolatey and related packages
 Set-ExecutionPolicy Bypass -Scope Process -Force;
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
