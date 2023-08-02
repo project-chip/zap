6a55ed00533df89d5809edf24be414482ae854c4
paulr34 <64710345+paulr34@users.noreply.github.com>
2023-08-02 14:51:49 -0400

    fix url parsing (#1076)
    * fixing parsing logic which works in studio

--- src-electron/rest/file-ops.js
+++ src-electron/rest/file-ops.js
@@ -28,6 +28,7 @@ const exportJs = require('../importexport/export.js')
 const path = require('path')
 const { StatusCodes } = require('http-status-codes')
 const querySession = require('../db/query-session.js')
+const querystring = require('querystring')
 const queryNotification = require('../db/query-notification.js')
 const dbEnum = require('../../src-shared/db-enum.js')
 const studio = require('../ide-integration/studio-rest-api')
@@ -41,12 +42,13 @@ const studio = require('../ide-integration/studio-rest-api')
 function httpPostFileOpen(db) {
   return async (req, res) => {
     let { zapFilePath, ideProjectPath } = req.body
-    if (req.body.search?.includes('filePath=%2F')) {
-      ideProjectPath = dbEnum.sessionKey.ideProjectPath
-      zapFilePath = req.body.search?.split('filePath=')
-      zapFilePath = zapFilePath[1]?.replaceAll('%2F', '//').trim()
+    let search = req.body.search
+    const query = new URLSearchParams(search)
+    let file = query.get('filePath')
+    if (file) {
+      zapFilePath = file
+      ideProjectPath = query.get('studioProject')
     }
-
     let name = ''
     if (zapFilePath) {
       if (studio.integrationEnabled(db, req.zapSessionId)) {
