250c953dbda8d4c8fa90a8fa33b57e0178158c22
Paul Regan <paregan@silabs.com>
2023-11-03 12:59:41 -0400

    remove debug code and update to node 20

--- src-electron/util/env.js
+++ src-electron/util/env.js
@@ -435,7 +435,7 @@ export function isMatchingVersion(versionsArray, providedVersion) {
  * @returns true or false, depending on match
  */
 export function versionsCheck() {
-  let expectedNodeVersion = ['v14.x.x', 'v16.x.x', 'v18.x.x']
+  let expectedNodeVersion = ['v14.x.x', 'v16.x.x', 'v18.x.x', 'v20.x.x']
   let expectedElectronVersion = ['17.4.x', '18.x.x', '24.x.x']
   let nodeVersion = process.version
   let electronVersion = process.versions.electron
