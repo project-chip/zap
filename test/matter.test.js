7544aa8af99c644c5a132a6e4ac3e58ca6e3510b
Paul Regan <paregan@silabs.com>
2024-08-12 16:46:38 -0400

    adding tests for forcedExternal plus matter sdk

---
+++ test/matter.test.js
@@ -0,0 +1,32 @@
+const matter = require('../src-electron/sdk/matter.js')
+const testUtil = require('./test-util')
+const env = require('../src-electron/util/env')
+const dbApi = require('../src-electron/db/db-api')
+const zclLoader = require('../src-electron/zcl/zcl-loader')
+
+const testFile = testUtil.matterTestFile.matterTest
+
+beforeAll(async () => {
+  env.setDevelopmentEnv()
+  let file = env.sqliteTestFile('gen-matter-1')
+  db = await dbApi.initDatabaseAndLoadSchema(
+    file,
+    env.schemaFile(),
+    env.zapVersion()
+  )
+  let ctx = await zclLoader.loadZcl(db, env.builtinMatterZclMetafile())
+  zclPackageId = ctx.packageId
+}, testUtil.timeout.long())
+
+test(
+  'forced external',
+  async () => {
+    let forcedExternal = await matter.getForcedExternalStorage(db, zclPackageId)
+    expect(forcedExternal.length).toBeGreaterThan(0)
+    forcedExternal.forEach((item) => {
+      expect(item).toHaveProperty('optionLabel')
+      expect(item).toHaveProperty('optionCategory')
+    })
+  },
+  testUtil.timeout.long()
+)
