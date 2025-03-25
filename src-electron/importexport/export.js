739112e47f2b4d45969fbae8772e7a49e1472c63
Dhruv Chandwani <dhchandw@silabs.com>
2025-03-25 17:42:14 -0400

    Fix: Handling paths on different drives in windows

--- src-electron/importexport/export.js
+++ src-electron/importexport/export.js
@@ -136,6 +136,10 @@ async function exportSessionPackages(db, sessionId, zapProjectFileLocation) {
         pathRelativity = dbEnum.pathRelativity.relativeToZap
       }
     }
+    if (path.isAbsolute(relativePath) && /^[A-Z]:\\/.test(relativePath)) {
+      // Handling Windows path when package is on different drive than zapProjectFile or user home
+      pathRelativity = dbEnum.pathRelativity.absolute
+    }
     let ret = {
       pathRelativity: pathRelativity,
       path: relativePath,
