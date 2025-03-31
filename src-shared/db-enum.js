e3aa9727bff3b8c627349d3a413526807d4f838a
Ethan.Z <Ethan.Zhou@silabs.com>
2025-03-31 16:03:24 -0400

    show warning and disable change for features with disabled cluster

--- src-shared/db-enum.js
+++ src-shared/db-enum.js
@@ -220,6 +220,7 @@ exports.packageMatch = {
 
 exports.feature = {
   name: {
+    status: 'status',
     enabled: 'enabled',
     deviceType: 'deviceType',
     cluster: 'cluster',
@@ -231,6 +232,7 @@ exports.feature = {
     description: 'description'
   },
   label: {
+    status: '',
     enabled: 'Enabled',
     deviceType: 'Device Type',
     cluster: 'Cluster',
