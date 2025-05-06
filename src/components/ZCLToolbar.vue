1a55ac36afbd2134d1cc530cbcc1f5a81cc19986
Paul Regan <paregan@silabs.com>
2025-05-06 15:05:45 -0400

    more cleanup

--- src/components/ZCLToolbar.vue
+++ src/components/ZCLToolbar.vue
@@ -248,12 +248,7 @@ export default {
       return this.$store.state.zap.query // Access the query string from Vuex
     },
     showSaveButton() {
-      console.log(this.query)
-      if (this.query['stsApplication']) {
-        return true
-      } else {
-        return false
-      }
+      return !!this.query?.stsApplication // Use optional chaining and double negation for clarity
     },
     isCoreDocumentationAvailable() {
       return (
