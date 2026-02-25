da76d9fb90101dda3ab9f47182ce6e4b7ef289b2
Tibor Kollár <Tibor.Kollar@silabs.com>
2026-02-25 13:32:53 +0100

    Fix duplication in config-loading

--- cypress/e2e/multiprotocol/config-loading.cy.js
+++ cypress/e2e/multiprotocol/config-loading.cy.js
@@ -19,16 +19,16 @@ import {
 Cypress.on('uncaught:exception', () => false)
 
 describe('Config loading – both Zigbee + Matter pre-loaded', () => {
-  it('should show both Zigbee and Matter ZCL packages', () => {
+  beforeEach(() => {
     const body = buildSessionAttemptResponse({
       zclProperties: [zigbeeZclPkg, matterZclPkg],
       zclGenTemplates: [zigbeeTemplatePkg, matterTemplatePkg]
-    })
-    stubConfigApis(body)
-
-    cy.visit('/')
-    cy.wait('@sessionAttempt')
-
+    });
+    stubConfigApis(body);
+    cy.visit('/');
+    cy.wait('@sessionAttempt');
+  });
+  it('should show both Zigbee and Matter ZCL packages', () => {
     cy.contains('Zigbee Cluster Library metadata').should('be.visible')
     cy.dataCy('zcl-package-checkbox').should('have.length', 2)
     cy.contains('td', 'zigbee').should('exist')
@@ -36,29 +36,11 @@ describe('Config loading – both Zigbee + Matter pre-loaded', () => {
   })
 
   it('should show both Zigbee and Matter generation templates', () => {
-    const body = buildSessionAttemptResponse({
-      zclProperties: [zigbeeZclPkg, matterZclPkg],
-      zclGenTemplates: [zigbeeTemplatePkg, matterTemplatePkg]
-    })
-    stubConfigApis(body)
-
-    cy.visit('/')
-    cy.wait('@sessionAttempt')
-
     cy.contains('Zap Generation Templates').should('be.visible')
     cy.get('[data-test="gen-template"]').should('have.length', 2)
   })
 
   it('should show multiprotocol warning when both ZCL categories are selected', () => {
-    const body = buildSessionAttemptResponse({
-      zclProperties: [zigbeeZclPkg, matterZclPkg],
-      zclGenTemplates: [zigbeeTemplatePkg, matterTemplatePkg]
-    })
-    stubConfigApis(body)
-
-    cy.visit('/')
-    cy.wait('@sessionAttempt')
-
     // Ensure both ZCL packages are checked
     cy.dataCy('zcl-package-checkbox').each(($el) => {
       if ($el.attr('aria-checked') !== 'true') {
@@ -70,15 +52,6 @@ describe('Config loading – both Zigbee + Matter pre-loaded', () => {
   })
 
   it('should warn about internal packages when nothing is selected', () => {
-    const body = buildSessionAttemptResponse({
-      zclProperties: [zigbeeZclPkg, matterZclPkg],
-      zclGenTemplates: [zigbeeTemplatePkg, matterTemplatePkg]
-    })
-    stubConfigApis(body)
-
-    cy.visit('/')
-    cy.wait('@sessionAttempt')
-
     // Uncheck every ZCL package
     cy.dataCy('zcl-package-checkbox').each(($el) => {
       if ($el.attr('aria-checked') === 'true') {
@@ -98,15 +71,6 @@ describe('Config loading – both Zigbee + Matter pre-loaded', () => {
   })
 
   it('should warn about misaligned categories when ZCL and template selections differ', () => {
-    const body = buildSessionAttemptResponse({
-      zclProperties: [zigbeeZclPkg, matterZclPkg],
-      zclGenTemplates: [zigbeeTemplatePkg, matterTemplatePkg]
-    })
-    stubConfigApis(body)
-
-    cy.visit('/')
-    cy.wait('@sessionAttempt')
-
     // Select both ZCL packages
     cy.dataCy('zcl-package-checkbox').each(($el) => {
       if ($el.attr('aria-checked') !== 'true') {
@@ -133,16 +97,17 @@ describe('Config loading – both Zigbee + Matter pre-loaded', () => {
 // Only Zigbee template pre-loaded (multiprotocol context)
 // ---------------------------------------------------------------------------
 describe('Config loading – only Zigbee template pre-loaded (multiprotocol)', () => {
-  it('should show config page with both ZCL packages but only Zigbee template', () => {
+  beforeEach(() => {
     const body = buildSessionAttemptResponse({
       zclProperties: [zigbeeZclPkg, matterZclPkg],
       zclGenTemplates: [zigbeeTemplatePkg]
-    })
-    stubConfigApis(body)
+    });
+    stubConfigApis(body);
+    cy.visit('/');
+    cy.wait('@sessionAttempt');
+  });
 
-    cy.visit('/')
-    cy.wait('@sessionAttempt')
-
+  it('should show config page with both ZCL packages but only Zigbee template', () => {
     cy.contains('Zigbee Cluster Library metadata').should('be.visible')
     cy.dataCy('zcl-package-checkbox').should('have.length', 2)
 
@@ -151,15 +116,6 @@ describe('Config loading – only Zigbee template pre-loaded (multiprotocol)', (
   })
 
   it('should warn about misaligned categories when both ZCL selected but only Zigbee template exists', () => {
-    const body = buildSessionAttemptResponse({
-      zclProperties: [zigbeeZclPkg, matterZclPkg],
-      zclGenTemplates: [zigbeeTemplatePkg]
-    })
-    stubConfigApis(body)
-
-    cy.visit('/')
-    cy.wait('@sessionAttempt')
-
     // Select both ZCL packages
     cy.dataCy('zcl-package-checkbox').each(($el) => {
       if ($el.attr('aria-checked') !== 'true') {
@@ -179,15 +135,6 @@ describe('Config loading – only Zigbee template pre-loaded (multiprotocol)', (
   })
 
   it('should not show multiprotocol warning when only Zigbee ZCL is selected', () => {
-    const body = buildSessionAttemptResponse({
-      zclProperties: [zigbeeZclPkg, matterZclPkg],
-      zclGenTemplates: [zigbeeTemplatePkg]
-    })
-    stubConfigApis(body)
-
-    cy.visit('/')
-    cy.wait('@sessionAttempt')
-
     // Select only the Zigbee ZCL package (first one)
     cy.dataCy('zcl-package-checkbox').each(($el, index) => {
       const checked = $el.attr('aria-checked') === 'true'
@@ -206,16 +153,16 @@ describe('Config loading – only Zigbee template pre-loaded (multiprotocol)', (
 // No templates pre-loaded (multiprotocol context)
 // ---------------------------------------------------------------------------
 describe('Config loading – no templates pre-loaded (multiprotocol)', () => {
-  it('should show config page with both ZCL packages but empty template table', () => {
+  beforeEach(() => {
     const body = buildSessionAttemptResponse({
       zclProperties: [zigbeeZclPkg, matterZclPkg],
       zclGenTemplates: []
-    })
-    stubConfigApis(body)
-
-    cy.visit('/')
-    cy.wait('@sessionAttempt')
-
+    });
+    stubConfigApis(body);
+    cy.visit('/');
+    cy.wait('@sessionAttempt');
+  });
+  it('should show config page with both ZCL packages but empty template table', () => {
     cy.contains('Zigbee Cluster Library metadata').should('be.visible')
     cy.dataCy('zcl-package-checkbox').should('have.length', 2)
     cy.contains('td', 'zigbee').should('exist')
@@ -226,15 +173,6 @@ describe('Config loading – no templates pre-loaded (multiprotocol)', () => {
   })
 
   it('should display warning about internal fallback packages', () => {
-    const body = buildSessionAttemptResponse({
-      zclProperties: [zigbeeZclPkg, matterZclPkg],
-      zclGenTemplates: []
-    })
-    stubConfigApis(body)
-
-    cy.visit('/')
-    cy.wait('@sessionAttempt')
-
     cy.dataCy('zcl-package-checkbox').first().click({ force: true })
 
     cy.contains(
@@ -243,15 +181,6 @@ describe('Config loading – no templates pre-loaded (multiprotocol)', () => {
   })
 
   it('should still show the submit button so user can proceed without templates', () => {
-    const body = buildSessionAttemptResponse({
-      zclProperties: [zigbeeZclPkg, matterZclPkg],
-      zclGenTemplates: []
-    })
-    stubConfigApis(body)
-
-    cy.visit('/')
-    cy.wait('@sessionAttempt')
-
     cy.get('[data-test="login-submit"]').should('be.visible')
   })
 })
